b78c588c874230defbcbc74cfa9d0802c1276a64
Илья Соболев <i4sobolev@yandex.ru>
2023-04-15 17:26:59 +0200

    only pain and suffeing

---
+++ yacut/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
+
+
+class URLMapForm(FlaskForm):
+    original_link = StringField(
+        'Длинная ссылка',
+        validators=[DataRequired(message='Обязательное поле'),
+                    URL(message='Это не ссылка')],
+    )
+
+    custom_id = StringField(
+        'Ваш вариант короткой ссылки',
+        validators=[Length(1, 16),
+                    Optional(),
+                    Regexp(regex=r'^[a-zA-Z\d]{1,16}$',
+                           message='Только латиница и цифры')
+                    ]
+
+    )
+    submit = SubmitField('Создать')
