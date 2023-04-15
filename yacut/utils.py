bf1cbb54554dbca40d806c6543d01a135e610199
Илья Соболев <i4sobolev@yandex.ru>
2023-04-15 23:10:21 +0200

    f forms, clear er, think on api

--- yacut/utils.py
+++ yacut/utils.py
@@ -6,20 +6,15 @@ from .models import URLMap
 
 
 def is_unique_short_id(short_id):
-
+    """Check if short link is in the database"""
     link = URLMap.query.filter_by(short=short_id).first()
     return link is None
 
 
 def get_unique_short_id():
+    """Creates a unique short link and returns it to views"""
     characters = string.ascii_letters + string.digits
     while True:
         short_id = ''.join(random.choice(characters) for i in range(6))
         if is_unique_short_id(short_id):
             return short_id
-
-
-# def is_unique_short_id(short_id):
-
-#     link = URLMap.query.filter_by(short_id=short_id).first()
-#     return link is None
