cc3fbeebfa990a44b3025afb432d6b5c399d8434
Илья Соболев <i4sobolev@yandex.ru>
2023-04-16 14:57:44 +0200

    default

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os .getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='asidyfukahs23')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
